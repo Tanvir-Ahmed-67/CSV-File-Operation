f7c25e3e7971a0199ba0d8c0b41316e1b42a44a3
Tanvir-Ahmed-67 <tanvirahmed67@yahoo.com>
2022-10-30 16:09:58 +0600

    For deploying

--- src/main/java/com/ApiApplication.java
+++ src/main/java/com/ApiApplication.java
@@ -2,11 +2,18 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({ "com","com.api","com.api.controller","com.api.helper","com.api.model","com.api.repository","com.api.service"})
-public class ApiApplication {
+//@ComponentScan({ "com","com.api","com.api.controller","com.api.helper","com.api.model","com.api.repository","com.api.service"})
+public class ApiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
